97118a84ca8e909266c2b1d61b61686331817ef7
hikariNTU <18393696+hikariNTU@users.noreply.github.com>
2024-06-25 22:50:53 +0800

    fix: use popover for mobile

--- src/components/tooltip.tsx
+++ src/components/tooltip.tsx
@@ -1,42 +1,57 @@
 "use client";
 
 import {
-  Tooltip,
-  TooltipContent,
-  TooltipTrigger,
-  TooltipProvider,
-  TooltipArrow,
-  TooltipPortal,
-} from "@radix-ui/react-tooltip";
+  Popover,
+  PopoverArrow,
+  PopoverContent,
+  PopoverPortal,
+  PopoverTrigger,
+} from "@radix-ui/react-popover";
 import clsx from "clsx";
-import { PropsWithChildren, forwardRef } from "react";
+import { PropsWithChildren, forwardRef, useState } from "react";
 
 export const TooltipWrap = forwardRef<
-  React.ElementRef<typeof TooltipContent>,
+  React.ElementRef<typeof PopoverContent>,
   PropsWithChildren<{
     content: React.ReactNode;
     className?: string;
   }>
->(function TooltipWrap({ className, children, content, ...props }, ref) {
+>(function PopoverWrap({ className, children, content, ...props }, ref) {
+  const [open, setOpen] = useState(false);
+
+  const handleMouseEnter = () => {
+    setOpen(true);
+  };
+
+  const handleMouseLeave = () => {
+    setOpen(false);
+  };
+
   return (
-    <TooltipProvider>
-      <Tooltip delayDuration={200}>
-        <TooltipTrigger asChild>{children}</TooltipTrigger>
-        <TooltipPortal>
-          <TooltipContent
-            ref={ref}
-            sideOffset={4}
-            className={clsx(
-              "z-50 overflow-hidden rounded-md bg-main-900 px-3 py-1.5 text-main-100 shadow-md animate-in fade-in-0 zoom-in-95 data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=closed]:zoom-out-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2",
-              className,
-            )}
-            {...props}
-          >
-            {content}
-            <TooltipArrow className="fill-main-900" />
-          </TooltipContent>
-        </TooltipPortal>
-      </Tooltip>
-    </TooltipProvider>
+    <Popover open={open} onOpenChange={setOpen}>
+      <PopoverTrigger
+        onMouseEnter={handleMouseEnter}
+        onMouseLeave={handleMouseLeave}
+        asChild
+      >
+        {children}
+      </PopoverTrigger>
+      <PopoverPortal>
+        <PopoverContent
+          onMouseEnter={handleMouseEnter}
+          onMouseLeave={handleMouseLeave}
+          ref={ref}
+          sideOffset={4}
+          className={clsx(
+            "z-50 overflow-hidden rounded-md bg-main-900 px-3 py-1.5 text-main-100 shadow-md animate-in fade-in-0 zoom-in-95 data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=closed]:zoom-out-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2",
+            className,
+          )}
+          {...props}
+        >
+          {content}
+          <PopoverArrow className="fill-main-900" />
+        </PopoverContent>
+      </PopoverPortal>
+    </Popover>
   );
 });
